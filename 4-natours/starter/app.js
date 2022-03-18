e64cfcc1e70eed523a109453528f91e4189ff08b
tjsilver <thalia.silver@guardian.co.uk>
2022-03-18 17:47:50 +0000

    section 6 complete

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,42 +1,32 @@
-const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// 1) Middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json()); // middleware - function to modify incoming request data
+app.use(express.static(`${__dirname}/public`));
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-app.get('/api/v1/tours', (req, res) => {
-  // specify version of api
-  res.status(200).json({
-    status: 'success',
-    results: tours.length, // not part of JSON spec but nice to do
-    data: {
-      tours, // same as tours: tours
-    },
-  });
+app.use((req, res, next) => {
+  console.log('Hello from the middleware! 😎');
+  // don't forget next!
+  next();
 });
 
-app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('Done');
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-const port = 3000;
+// 3) Routes
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
